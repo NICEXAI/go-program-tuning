e81e4cec07729c11f48214a0394c5b96727b6ef1
Afeyer <1500527791@qq.com>
2022-03-16 15:50:37 +0800

    pref: optimize log output

--- program_unsupport.go
+++ program_unsupport.go
@@ -4,16 +4,16 @@
 package go_program_tuning
 
 import (
-	"fmt"
+	"errors"
 	"github.com/fatih/color"
 )
 
 func init() {
 	if err := Tuning(); err != nil {
-		color.Red("GoProgramTuning 目前仅支持 Linux 平台，该 SDK 将不会被启用")
+		color.Red(err.Error())
 	}
 }
 
 func Tuning() error {
-	return fmt.Errorf("该 SDK 目前只支持 Linux 平台运行，请在 Linux 环境下安装并执行")
+	return errors.New("GoProgramTuning 目前仅支持 Linux 平台，该 SDK 将不会被启用")
 }
